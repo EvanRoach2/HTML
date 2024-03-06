0daf8602de57bc27064c793d0e680235dd292bf5
EvanRoach2 <149097724+EvanRoach2@users.noreply.github.com>
2024-03-06 09:37:02 -0500

    gvhn

--- java/java.js
+++ java/java.js
@@ -12,3 +12,102 @@ function decreaseValue() {
   value--;
   document.getElementById('number').value = value;
 }
+document.querySelector(".example").style.backgroundColor = "blue";
+
+var dots = [],
+    mouse = {
+      x: 0,
+      y: 0
+    };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function(){
+    var n = document.createElement("div");
+    n.className = "trail";
+    document.body.appendChild(n);
+    return n;
+  }());
+};
+// The Dot.prototype.draw() method sets the position of 
+  // the object's <div> node
+Dot.prototype.draw = function() {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 12; i++) {
+  var d = new Dot();
+  dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+  // Make sure the mouse position is set everytime
+    // draw() is called.
+  var x = mouse.x,
+      y = mouse.y;
+  
+  // This loop is where all the 90s magic happens
+  dots.forEach(function(dot, index, dots) {
+    var nextDot = dots[index + 1] || dots[0];
+    
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * .6;
+    y += (nextDot.y - dot.y) * .6;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  //event.preventDefault();
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+// animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+  draw();
+  requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
+
+function displayText() {
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+}
+let hour = new Date().getHours();
+if (hour < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+var name = prompt('What is your name?');
+if(name=='Joe') //If the user’s name is ‘Joe’
+{
+    alert('Wow, cool – my name is Joe too!');
+}
+else if(name=='Fred') //If the user’s name is ‘Fred’
+{
+    alert('Fred is a pretty generic name...');
+}
+else if(name=='John') //If the user’s name is ‘John’
+{
+    alert('Alright, John?');
+}
+else if(name=='George') //If the user’s name is ‘George’
+{
+    alert('George is a pretty decent name.');
+}
+else
+{
+    alert('I have no opinion on your name.');
+}
